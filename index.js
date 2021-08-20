d655b2ef6b2d47034e3da062c7b55b0818807f64
Roxanne <89050736+stackdev-web@users.noreply.github.com>
2021-08-20 19:24:23 +0000

    count heads

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
-function flip(){
+function flip(repeat){
+  var countHeads = 0;
 
-for (let i = 0; i < 10; i++){
+for (let i = 0; i < repeat; i++){
 
 var coinFlip = Math.round(Math.random());
+
 if (coinFlip === 1){
   console.log('The flip reveals', 'heads', '.');
+  countHeads = countHeads + 1;
 }
 
 if (coinFlip === 0){
   console.log('The flip reveals', 'tails', '.');
+    }
+  }
+
+  return(countHeads/repeat);
 }
-}
-}
-flip();
+flip(100);
